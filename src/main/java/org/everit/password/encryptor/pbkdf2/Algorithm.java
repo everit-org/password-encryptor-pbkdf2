70aed74fa6f6df287ea79a5c2236852d57c78f01
zsigmond.czine <zsigmond.czine@everit.biz>
2015-10-06 17:34:05 +0200

    fixed package name.

--- src/main/java/org/everit/password/encryptor/pbkdf2/Algorithm.java
+++ src/main/java/org/everit/password/encryptor/pbkdf2/Algorithm.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.everit.osgi.password.encryptor.pbkdf2;
+package org.everit.password.encryptor.pbkdf2;
 
 import java.util.Collections;
 import java.util.HashMap;
